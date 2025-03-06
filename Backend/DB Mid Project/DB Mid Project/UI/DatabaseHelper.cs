228cea8e600d27f05ef32a0ab6b9ee6c8e517d8f
ZainUET <zain41uet@gmail.com>
2025-03-06 06:36:39 +0500

    Add files via upload

---
+++ Backend/DB Mid Project/DB Mid Project/UI/DatabaseHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DB_Mid_Project
+{
+    public class DatabaseHelper
+    {
+        private String serverName = "127.0.0.1";
+        private String port = "3306";
+        private String databaseName = "fms";
+        private String databaseUser = "root";
+        private String databasePassword = "zain.8773";
+        private DatabaseHelper() { }
+        private static DatabaseHelper? _instance;
+        public static DatabaseHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DatabaseHelper();
+                return _instance;
+            }
+        }
+        public MySqlConnection getConnection()
+        {
+            string connectionString = $"server={serverName};port={port};user={databaseUser};database={databaseName};password={databasePassword};SslMode=Required;";
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+        public MySqlDataReader getData(string query)
+        {
+            using (var connection = getConnection())
+            {
+                using (var command = new MySqlCommand(query, getConnection()))
+                {
+                    return command.ExecuteReader();
+                }
+            }
+        }
+        public int Update(string query)
+        {
+            using (var connection = getConnection())
+            {
+                using (var command = new MySqlCommand(query, getConnection()))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        internal object Query(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
