284f7ae803f0e4bf5dd015a87edce1676d7aa6a7
ZainUET <zain41uet@gmail.com>
2025-03-22 00:41:42 +0500

    Add files via upload

---
+++ Backend/DB Mid Project/DB Mid Project/UI/DepartmentHeadDashborad.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Mid_Project.UI
+{
+    public partial class DepartmentHeadDashborad : Form
+    {
+        public DepartmentHeadDashborad()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Login loginForm = new Login();
+            this.Hide();
+            loginForm.ShowDialog();
+            this.Close();
+
+        }
+
+        private void linkLabel8_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ApproveRejectRequests loginForm = new ApproveRejectRequests();
+            this.Hide();
+            loginForm.ShowDialog();
+            this.Close();
+
+
+        }
+    }
+}
